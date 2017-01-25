5aa7d14860c6f01a9bc2e76f90d2d73006a57e78
Blake Ingram <blakeingram@gmail.com>
2017-01-25 11:38:05 +0800

    Render game board with character at starting position and enemies generated. All stationay. Add 4th row of stone toward bottom of screen.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = 0; //for now speed not working
+
 };
 
 // Update the enemy's position, required method for game
@@ -24,12 +28,35 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y, sprite) {
+  this.sprite = 'images/char-boy.png';
+  this.x = 202;
+  this.y = 405;
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [
+  new Enemy(0,70,0),
+  new Enemy(0,150,0),
+  new Enemy(0,225,0)
+];
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
