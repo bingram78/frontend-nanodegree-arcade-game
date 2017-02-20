dcf05bcb7778ef71822286fce07bf5fbbf84e1ac
Blake Ingram <blakeingram@gmail.com>
2017-02-20 11:08:19 +0800

    Comment on necessary classes and functions. Adjust quote types and indenting.

--- js/app.js
+++ js/app.js
@@ -10,44 +10,45 @@ var imgDim = {w:101, h:171};
 
 // Enemies our player must avoid
 var Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    // Loads the enemy-bug image.
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    // Formula for randomizing speed of enemy.
-    this.speed = Math.floor((Math.random()*4)+2);
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  // Loads the enemy-bug image.
+  this.sprite = 'images/enemy-bug.png';
+  this.x = x;
+  this.y = y;
+  // Formula for randomizing speed of enemy.
+  this.speed = Math.floor((Math.random()*4)+2);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    //******* alters speed of enemy bugs **********
-    this.x = this.x + (105 * dt * this.speed);
-    //if enemy bug goes off screen reset it to update.
-    if (this.x > 650) {
-		    this.reset()
-      };
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  //******* alters speed of enemy bugs **********
+  this.x = this.x + (105 * dt * this.speed);
+  //if enemy bug goes off screen reset it to update.
+  if (this.x > 650) {
+	    this.reset()
+    };
 };
 
+//Resets each Enemy and randomizes y position when it goes off screen.
 Enemy.prototype.reset = function() {
-    //starts the enemy off screen
-    this.x = -100;
-    var yValues = [65, 145, 230, 310];
-    this.y = yValues[Math.floor((Math.random() * 4))];
-    this.speed;
+  //starts the enemy off screen
+  this.x = -100;
+  var yValues = [65, 145, 230, 310];
+  this.y = yValues[Math.floor((Math.random() * 4))];
+  this.speed;
 }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
@@ -61,14 +62,15 @@ var Player = function(x, y, sprite) {
   this.y = startY;
 };
 
+//Update function handling player position for restart and messages.
 Player.prototype.update = function(dt) {
   if (this.y > -83 && this.y < 0) {
-    document.getElementById("winner").innerHTML ="Congrats! Press up to start over"
+    document.getElementById('winner').innerHTML ='Congrats! Press up to start over'
     };
   if (this.y < -83){
     this.x = startX;
     this.y = startY;
-    document.getElementById("winner").innerHTML =""
+    document.getElementById('winner').innerHTML =''
   };
   //Collision detection algorithm.
   allEnemies.forEach(function(enemy) {
@@ -77,13 +79,14 @@ Player.prototype.update = function(dt) {
         player.y < enemy.y + 40 &&
         player.y > enemy.y) {
           player.reset();
-          document.getElementById("winner").innerHTML ="Try Again!"
+          document.getElementById('winner').innerHTML ='Try Again!'
         };
     });
 };
 
+//Render player when game loads.
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 //Handling input for user and limiting to only direction keys.
@@ -112,6 +115,7 @@ Player.prototype.handleInput = function(direction) {
     }
 }
 
+//Resets player to bottom of the screen in the grass.
 Player.prototype.reset = function() {
   this.x = startX;
   this.y = startY;
@@ -134,12 +138,12 @@ var player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
